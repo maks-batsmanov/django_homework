d9cf0d87a2d4b9b30d5ce3aaf184a0a63e30caf8
Максим Бацманов <markgusev995@gmail.com>
2026-04-11 09:13:12 +0300

    добавлен путь / в urlpatterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
+    path('', views.home_view, name='home'),
     path("home/", views.home_view, name='home'),
     path("contacts/", views.contacts_view, name='contacts')
 ]
