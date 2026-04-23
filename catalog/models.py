c53949814d16b6e8afad114d76b77717b3db6e0e
Максим Бацманов <markgusev995@gmail.com>
2026-04-24 00:36:23 +0300

    исправил модели, создал и применил миграции

--- catalog/models.py
+++ catalog/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name='Изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    price = models.DecimalField(verbose_name='Стоимость покупки')
+    price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Стоимость покупки')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
 
