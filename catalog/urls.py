97b3bf399afbab6cd78922f10d6ccb0f758994e5
Максим Бацманов <markgusev995@gmail.com>
2026-04-29 21:59:56 +0300

    добавлены пути

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path("home/", views.home_view, name='home'),
     path("contacts/", views.contacts_view, name='contacts'),
-    path('product_detail/<int:pk>/', views.product_detail, name='product_detail')
+    path('product_detail/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/create/', views.product_create_view, name='product_create'),
 ]
