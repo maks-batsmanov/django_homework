0ab74e1824a6d263a495b0f162343e99d027d204
Максим Бацманов <markgusev995@gmail.com>
2026-05-01 00:11:45 +0300

    исправлены маршруты

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path, include
-from . import views
-
+from django.urls import path
+from .views import ProductDetailView, ProductCreateView, ProductListView, ContactsView
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path("home/", views.home_view, name='home'),
-    path("contacts/", views.contacts_view, name='contacts'),
-    path('product_detail/<int:pk>/', views.product_detail, name='product_detail'),
-    path('product/create/', views.product_create_view, name='product_create'),
+
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('', ProductListView.as_view(), name='home'),
+    path('home/', ProductListView.as_view(), name='home'),
+    path("contacts/", ContactsView.as_view(), name='contacts'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/create/', ProductCreateView.as_view(), name='form_add_product')
 ]
