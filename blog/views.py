b3d5bbe3a34afff27c94daa144ff63d0941e82e8
Максим Бацманов <markgusev995@gmail.com>
2026-05-01 14:03:24 +0300

    созданы контроллеры

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, FormView, CreateView
 
-# Create your views here.
+from blog.forms import AddEntryForm
+from blog.models import BlogEntry
+
+
+class BlogListView(ListView):
+    model = BlogEntry
+
+    def get_queryset(self):
+        queryset = BlogEntry.objects.all()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(publication_attribute=True)
+        return queryset
+
+
+class BlogCreateView(CreateView):
+    model = BlogEntry
+    template_name = 'blog/blogentry_create.html'
+    form_class = AddEntryForm
+    success_url = reverse_lazy('blog:list')
