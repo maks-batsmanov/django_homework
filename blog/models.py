587831deb05206980f1b7eb85ca77b7274c7156e
Максим Бацманов <markgusev995@gmail.com>
2026-05-01 11:37:50 +0300

    создана модель BlogEntry

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+
+class BlogEntry(models.Model):
+    heading = models.CharField(max_length=150, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержание')
+    preview = models.ImageField(upload_to='blog/', blank=True, null=True, verbose_name='Изображение')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    publication_attribute = models.BooleanField(default=False, verbose_name='Признак публикации')
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
+
+    class Meta:
+        verbose_name = 'статья'
+        verbose_name_plural = 'статьи'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.heading
